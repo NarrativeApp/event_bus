e8ef7f4b0c51301f4f31a2be5943b0e17d56f7ba
Steffan Levet <steffan.g.levet@gmail.com>
2018-10-11 08:26:02 +1300

    Listnener can now be a pid to an active GenServer

--- lib/event_bus/services/notification.ex
+++ lib/event_bus/services/notification.ex
@@ -8,14 +8,8 @@ defmodule EventBus.Service.Notification do
   alias EventBus.Manager.Subscription, as: SubscriptionManager
   alias EventBus.Model.Event
 
-  @typep event :: EventBus.event()
-  @typep event_shadow :: EventBus.event_shadow()
-  @typep listener :: EventBus.listener()
-  @typep listener_list :: EventBus.listener_list()
-  @typep topic :: EventBus.topic()
-
   @doc false
-  @spec notify(event()) :: :ok
+  @spec notify(Event.t()) :: no_return()
   def notify(%Event{id: id, topic: topic} = event) do
     listeners = SubscriptionManager.subscribers(topic)
 
@@ -27,19 +21,16 @@ defmodule EventBus.Service.Notification do
 
       notify_listeners(listeners, {topic, id})
     end
-
-    :ok
   end
 
-  @spec notify_listeners(listener_list(), event_shadow()) :: :ok
+  @spec notify_listeners(list(), tuple()) :: no_return()
   defp notify_listeners(listeners, event_shadow) do
     Enum.each(listeners, fn listener ->
       notify_listener(listener, event_shadow)
     end)
-    :ok
   end
 
-  @spec notify_listener(listener(), event_shadow()) :: no_return()
+  @spec notify_listener(tuple(), tuple()) :: no_return()
   defp notify_listener({listener, config}, {topic, id}) do
     listener.process({config, topic, id})
   rescue
@@ -48,20 +39,27 @@ defmodule EventBus.Service.Notification do
       ObservationManager.mark_as_skipped({{listener, config}, {topic, id}})
   end
 
+  @spec notify_listener(module(), tuple()) :: no_return()
   defp notify_listener(listener, {topic, id}) do
-    listener.process({topic, id})
+    cond do
+      is_pid(listener) ->
+        GenServer.cast(listener, {topic, id})
+
+      true ->
+        listener.process({topic, id})
+    end
   rescue
     error ->
       log_error(listener, error)
       ObservationManager.mark_as_skipped({listener, {topic, id}})
   end
 
-  @spec registration_status(topic()) :: String.t()
+  @spec registration_status(atom()) :: String.t()
   defp registration_status(topic) do
     if EventBus.topic_exist?(topic), do: "", else: " doesn't exist!"
   end
 
-  @spec warn_missing_topic_subscription(topic()) :: no_return()
+  @spec warn_missing_topic_subscription(atom()) :: no_return()
   defp warn_missing_topic_subscription(topic) do
     msg =
       "Topic(:#{topic}#{registration_status(topic)}) doesn't have subscribers"
@@ -71,7 +69,15 @@ defmodule EventBus.Service.Notification do
 
   @spec log_error(module(), any()) :: no_return()
   defp log_error(listener, error) do
-    msg = "#{listener}.process/1 raised an error!\n#{inspect(error)}"
+    msg =
+      cond do
+        is_pid(listener) ->
+          "#{inspect(listener)}.process/1 raised an error!\n#{inspect(error)}"
+
+        true ->
+          "#{listener}.process/1 raised an error!\n#{inspect(error)}"
+      end
+
     Logger.info(msg)
   end
 end
